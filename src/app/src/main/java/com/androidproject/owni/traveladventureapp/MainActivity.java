89cc75b067901fba96564e11c25d71329a8f94a8
ownI <ownI>
2017-05-09 14:40:37 +0200

    Add working navigation panel

--- src/app/src/main/java/com/androidproject/owni/traveladventureapp/MainActivity.java
+++ src/app/src/main/java/com/androidproject/owni/traveladventureapp/MainActivity.java
@@ -9,6 +9,10 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.androidproject.owni.traveladventureapp.database.DBLocation;
 
@@ -35,7 +39,42 @@ public class MainActivity extends FragmentActivity implements TravelMapFragment.
     }
 
     @Override
-    public  void onFragmentInteraction(Uri uri){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.current_travel:
+                showCurrentTravel();
+                return true;
+            case R.id.travel_list:
+                showTravelList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void showCurrentTravel() {
+        TravelMapFragment mapFragment = new TravelMapFragment();
+        //mapFragment.setArguments(args);
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map, mapFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void showTravelList() {
+        // TODO
+        // show travel list
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
         // to do if needed interactions
     }
 
